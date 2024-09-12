0ed7288888ce03fdd0dfeb9287bfe48908d7b4fd
luxlook <luxlook@peonybooks.com>
2024-09-12 10:24:52 +0900

    SearchProductLV url 추가

--- MarketKurly/product/urls.py
+++ MarketKurly/product/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProductLV, ProductDV, NewProductLV, MainCategoryLV, SubCategoryLV
+from .views import ProductLV, ProductDV, NewProductLV, MainCategoryLV, SubCategoryLV, SearchProductLV
 
 app_name = 'product'
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('detail/<int:pk>/', ProductDV.as_view(), name='product_detail'),
     path('new/', NewProductLV.as_view(), name='new_product'),
     path('category/<int:category_id>/', MainCategoryLV.as_view(), name='main_category'),
-    path('subcategory/<int:subcategory_id>/', SubCategoryLV.as_view(), name='sub_category')
+    path('subcategory/<int:subcategory_id>/', SubCategoryLV.as_view(), name='sub_category'),
+    path('search/', SearchProductLV.as_view(), name='search_products'),
 ] 
