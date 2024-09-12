0d867127f6c9860f52cca062d838a6b4b1b9d112
www3drbiz <166705926+www3drbiz@users.noreply.github.com>
2024-09-12 09:08:47 +0900

    Update settings.py
    settings에 AUTH_USER_MODEL = 'users.User' 를 추가하였습니다.

--- MarketKurly/mysite/settings.py
+++ MarketKurly/mysite/settings.py
@@ -126,6 +126,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+AUTH_USER_MODEL = 'users.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
