f2a928f8292ccfd65f454009ef2da21445d4182d
www3drbiz <166705926+www3drbiz@users.noreply.github.com>
2024-09-12 14:24:08 +0900

    Update urls.py
    , 가 빠져서 추가합니다.

--- MarketKurly/mysite/urls.py
+++ MarketKurly/mysite/urls.py
@@ -21,6 +21,6 @@ from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('product/', include("product.urls"))
+    path('product/', include("product.urls")),
     path('users/', include("users.urls")),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
